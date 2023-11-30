01f81d2c643a2a94c751a7e635a608188afa6a97
user <user@DESKTOP-NE0I5RC>
2023-11-30 09:54:43 +0530

    Add project files.

---
+++ Controllers/deptController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Empmanagement_MVC.Controllers
+{
+    public class deptController : Controller
+    {
+        public IActionResult PostDetails()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/empController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Empmanagement_MVC.Controllers
+{
+    public class empController : Controller
+    {
+        public IActionResult PostDetails()
+        {
+            return View();
+        }
+    }
+}
